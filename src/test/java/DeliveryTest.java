f7a28667e7ac9ad9d4d9fd9c4dc60dd472924cd7
Roman <borisenko0893@gmail.com>
2023-11-13 21:02:59 +0300

    add good jar file

--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -1,5 +1,4 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
